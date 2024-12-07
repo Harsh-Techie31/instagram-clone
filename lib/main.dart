b392c011861af23845247a905a1af4b6a82e391e
Harsh - <harshchoudhary3113@gmail.com>
2024-12-07 21:18:08 +0530

    set firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/firebase_options.dart';
 import 'package:instagram_flutter/responsive/mobilescreenlayout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_builder.dart';
 import 'package:instagram_flutter/responsive/webscreenlayout.dart';
-import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   // debugPaintSizeEnabled : false;
   runApp(const MyApp());
 }
